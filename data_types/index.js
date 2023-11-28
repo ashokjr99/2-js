e820f4c68c909e718609a972a624b89f06eef4cf
ashokjr99 <ashoksharmajr@gmail.com>
2023-11-27 19:12:57 -0600

    added datat types

--- data_types/index.js
+++ data_types/index.js
@@ -1,1 +1,40 @@
-console.log("Test");
+console.log("hello world");
+
+/*
+?    -    Javascript
+    - found in 1996 by Brandon Eich
+    - client sided language (runs in the browser)
+    - interpreted language (not compiled)
+    - functional language 
+    - object oriented (OOP)
+    - prototype-based
+    - weakly typed (dynamic)
+    - multi paradigm languages
+    - ECMA script - organization for setting the language standards
+*/
+
+/*
+?   -   Console Object
+    - gives us access to the browser or node's debugging console
+    - allows the developer the view the output from their program
+*/
+
+//! 2+2 does not show us the output
+console.log(2 + 2); //shows the output of 4
+
+/*
+
+JS Data Types:
+    Primitives
+        * strings
+        * numbers
+        * boolean
+        * null
+        * undefined
+        * NaN (Not a Number)
+    References
+        * arrays
+        * objects
+        * maps
+        * sets
+*/
